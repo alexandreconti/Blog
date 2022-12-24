b71d7ecee6a72729ad118ab11bff53c50c2c76da
Alexandre De Conti <alexandreconti@gmail.com>
2022-12-24 11:41:32 -0300

    Dapper Contrib

--- Program.cs
+++ Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Blog.Models;
+using Dapper.Contrib.Extensions;
+using Microsoft.Data.SqlClient;
+
+namespace Blog
+{
+    public class Program
+    {
+        private const string CONNECTION_STRING = @"Server=localhost,1433;Database=Blog;User ID=sa;Password=eg78IKnr58J8Scy7";
+
+        static void Main()
+        {
+            ReadUsers();
+        }
+
+        public static void ReadUsers()
+        {
+            using (var connection = new SqlConnection(CONNECTION_STRING))
+            {
+                var users = connection.GetAll<User>();
+
+                foreach (var user in users)
+                {
+                    Console.WriteLine(user.Name);
+                }
+            }
+        }
+    }
+}
